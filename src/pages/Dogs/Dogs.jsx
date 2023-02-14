4dc8e86024e366f94faa344e0bb52e0179edb097
Leonid Safonov <safonov.lenia2017@yandex.ru>
2023-02-14 15:25:59 +0300

    File organization + installed redux&router

---
+++ src/pages/Dogs/Dogs.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Dogs() {
+   return <div>Dogs</div>;
+}
---
+++ src/pages/Foxes/Foxes.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Foxes() {
+   return <div>Foxes</div>;
+}
